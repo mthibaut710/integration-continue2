f9fc13b5bf1bee691198591b257b32db00cb3cf0
Hugo Rollier <hugo.rollier@ecoles-epsi.net>
2024-10-25 13:42:57 +0200

    CI : vitest

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-const express = require("express");
-const auth = require("./modules/authentication");
+const express = require('express');
+const auth = require('./modules/authentication');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.get("/auth/:secret", (req, res) => {
+app.get('/auth/:secret', (req, res) => {
   const { secret } = req.params;
   const response = auth(secret);
 
-  console.log(secret);
   res.status(response.status).send(response.message);
 });
 
