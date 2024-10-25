bf50be963b7794a0201c09a2ea8f7c5e7da2a184
Hugo Rollier <hugo.rollier@ecoles-epsi.net>
2024-10-25 12:04:44 +0200

    CI : add things

--- src/index.js
+++ src/index.js
@@ -15,8 +15,3 @@ app.get("/auth/:secret", (req, res) => {
   console.log(secret);
   res.status(response.status).send(response.message);
 });
-
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Example app listening on http://localhost:${port}`);
-});
