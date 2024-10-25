31d930ac06cf6e1ec013d478d787d32209728f3b
[Mirooww] <[jordanlansoypro@gmail.com]>
2024-10-25 10:38:20 +0200

    Ajout du fichier gitlab-ci.yml

--- src/modules/authentication.js
+++ src/modules/authentication.js
@@ -1,27 +1,28 @@
-const PREFIX = 'secret-';
+const PREFIX = "secret-";
+const dotenv = require("dotenv").config();
 
 function isSecret(secret) {
-  return secret.startsWith(PREFIX);
+   return secret.startsWith(PREFIX);
 }
 
 function isAuth(secret) {
-  const validPassphrase = process.env.PASSPHRASE || 'mysecretphrase';
-  const passphrase = secret.split(PREFIX)[1];
+   const validPassphrase = process.env.PASSPHRASE || "mysecretphrase";
+   const passphrase = secret.split(PREFIX)[1];
 
-  return passphrase === validPassphrase;
+   return passphrase === validPassphrase;
 }
 
 function auth(secret) {
-  if (secret) {
-    if (isSecret(secret)) {
-      if (isAuth(secret)) {
-        return { status: 200, message: 'Success' };
+   if (secret) {
+      if (isSecret(secret)) {
+         if (isAuth(secret)) {
+            return { status: 200, message: "Success" };
+         }
+         return { status: 403, message: "Forbidden." };
       }
-      return { status: 403, message: 'Forbidden.' };
-    }
-    return { status: 400, message: 'Bad request.' };
-  }
-  return { status: 401, message: 'No secret provided.' };
+      return { status: 400, message: "Bad request." };
+   }
+   return { status: 401, message: "No secret provided." };
 }
 
 module.exports = auth;
