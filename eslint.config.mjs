78fc30b6142328895a1cf758387dc034b035d9b4
Your Name <you@example.com>
2025-09-26 16:05:46 +0200

    ...

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,21 @@
-import js from "@eslint/js";
+// eslint.config.mjs
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import pluginJs from "@eslint/js";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  {
+    ignores: ["**/*.test.js"],
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+    globals: { ...globals.node }, 
+  },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "no-console": "error",
+    },
+  },
+];
