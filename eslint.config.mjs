2bbed25a2f46f1a64d1f6a19b852e823bd3b269e
Luc <luc.rousseaupro@gmail.com>
2025-10-20 11:45:47 +0200

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import pluginJs from "@eslint/js";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    ignores: ["**/*.test.js"],
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "no-console": "error",
+    },
+  },
+]);
